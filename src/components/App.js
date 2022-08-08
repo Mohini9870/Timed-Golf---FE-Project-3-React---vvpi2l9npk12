0559c57b56f26a15aa75ff7051ae0fda4b9a7ef1
Mohini9870 <88155103+Mohini9870@users.noreply.github.com>
2022-08-08 12:51:19 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,54 @@ class Timer extends React.Component {
   constructor(props) {
     super(props);
     this.state = { time: 0, x: 0, y: 0 };
+     this.handleClick = this.handleClick.bind(this);
+    this.clock = this.clock.bind(this);
+    this.handleEvent = this.handleEvent.bind(this);
   }
+  handleEvent(event) {
+    switch (event.keyCode) {
+      case 37:
+        this.setState({ x: this.state.x - 5 });
+        // console.log("case ", this.state.x, this.state.y);
+        break;
+      case 38:
+        this.setState({ y: this.state.y - 5 });
+        // console.log("case ", this.state.x, this.state.y);
+        break;
+      case 39:
+        this.setState({ x: this.state.x + 5 });
+        // console.log("case ", this.state.x, this.state.y);
+        break;
+      case 40:
+        this.setState({ y: this.state.y + 5 });
+        // console.log("case ", this.state.x, this.state.y);
+        break;
+      default:
+        break;
+    }
+    // console.log("case k bahar ", this.state.x, this.state.y);
+  }
+  handleClick() {
+    if (this.state.time === 0) {
+      document.addEventListener("keydown", this.handleEvent);
+      this.timer = setInterval(() => {
+        this.clock();
+      }, 1000);
+    }
+  }
+ 
   componentDidMount() {
     
   }
-
+ componentDidUpdate() {
+    if (this.state.x === this.state.y && this.state.x === 250) {
+      document.removeEventListener("keydown", this.handleEvent);
+      clearInterval(this.timer);
+    }
+  }
+  clock() {
+    this.setState({ time: this.state.time + 1 });
+  }
   componentWillUnmount() {
     
   }
@@ -18,6 +61,19 @@ class Timer extends React.Component {
   render() {
     return (
  <>
+      <div
+          className="ball"
+          style={{
+            left: this.state.x + "px",
+            top: this.state.y + "px",
+            position: "absolute",
+          }}
+        ></div>
+        <div className="hole"></div>
+        <h1 className="heading-timer">{this.state.time}</h1>
+        <button className="start" onClick={this.handleClick}>
+          start the game
+        </button>
 </>
     );
   }
